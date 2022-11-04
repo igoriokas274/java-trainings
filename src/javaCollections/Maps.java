2d5a8ec49919283c879cd591fb8a57f1c0ab0d0b
Igor Lysenkov <igor@homebox.dev>
2022-11-04 23:48:07 +0200

    Maps.java Class created

---
+++ src/javaCollections/Maps.java
@@ -0,0 +1,42 @@
+package javaCollections;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Maps {
+
+    public static void main(String[] args) {
+
+        HashMap<Integer, String> hashMap = new HashMap<>(); // UNSORTED MAP OR SORTING NOT GUARANTEED
+        LinkedHashMap<Integer, String> linkedHashMap = new LinkedHashMap<>(); // AS ADDED MAP
+        TreeMap<Integer, String> treeMap = new TreeMap<>(); // SORTED MAP
+
+        listMap(hashMap);
+        System.out.println();
+        listMap(linkedHashMap);
+        System.out.println();
+        listMap(treeMap);
+
+    }
+
+    public static void listMap(Map<Integer, String> map) {
+
+        map.put(1, "Toyota");
+        map.put(4, "Dodge");
+        map.put(7, "Opel");
+        map.put(3, "Honda");
+        map.put(8, "Renault");
+        map.put(2, "Fiat");
+        map.put(5, "Ford");
+        map.put(9, "BMW");
+        map.put(6, "Seat");
+        map.put(0, "Skoda");
+
+        for (Integer key : map.keySet()) {
+            String value = map.get(key);
+            System.out.println(key + " : " + value);
+        }
+    }
+}
