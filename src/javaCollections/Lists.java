1450084501b3e6e0e7c9b4a54f609cfe7ccadfbb
Igor Lysenkov <igor@homebox.dev>
2022-11-05 13:26:52 +0200

    Arrays.java renamed to Lists.java Lists.java supplemented with additional solutions

--- src/javaCollections/Lists.java
+++ src/javaCollections/Lists.java
@@ -4,12 +4,37 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
-public class Arrays {
+public class Lists {
 
     public static void main(String[] args) {
 
+        // DUPLICATED VALUES ARE ACCEPTABLE
         ArrayList<Integer> arrayList = new ArrayList<>();
         LinkedList<Integer> linkedList = new LinkedList<>();
+
+        arrayList.add(2);
+        arrayList.add(5);
+        arrayList.add(33);
+        arrayList.add(9);
+        arrayList.add(2);
+
+        for (Integer integer : arrayList) {
+            System.out.println("Item " + integer);
+        }
+
+        System.out.println();
+
+        linkedList.add(2);
+        linkedList.add(5);
+        linkedList.add(33);
+        linkedList.add(9);
+        linkedList.add(2);
+
+        for (Integer integer : linkedList) {
+            System.out.println("Item " + integer);
+        }
+
+        System.out.println();
 
         System.out.println("Time spent for adding values in ArrayList " + doTimeCalculation(arrayList) + " ms");
         System.out.println("Time spent for adding values in LinkedList " + doTimeCalculation(linkedList) + " ms");
@@ -27,7 +52,6 @@ public class Arrays {
         long end = System.currentTimeMillis();
 
         return (end - start);
-
 
     }
 }
